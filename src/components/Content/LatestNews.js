09f0f7fb6164d651a34eed9e0596efedc8beb423
Muhammad Rabi <vonrabi07@yahoo.com>
2022-10-13 12:41:04 +0200

    responsive fixes

--- src/components/Content/LatestNews.js
+++ src/components/Content/LatestNews.js
@@ -8,10 +8,10 @@ const LatestNews = () => {
   return (
     <>
       <WidgetLayout>
-        <h3 className="content-headline">latest news</h3>
-        <div className="flex flex-col sm:flex-row text-center md:text-left justify-between items-center py-4 border-b border-slate-100 ">
-          <img src={pic1} className="w-20 h-14 rounded-md" alt="pic1" />
-          <article className="capitalize">
+        <h2 className="content-headline">latest news</h2>
+        <div className="flex flex-col sm:flex-row items-center py-4 text-center sm:text-left border-b border-slate-100 ">
+          <img src={pic1} className="w-36 h-20 mr-4 rounded-md" alt="pic1" />
+          <article className="capitalize grow">
             <h5 className="font-bold mt-4 sm:mt-1">changed the design</h5>
             <span className="text-sm text-zinc-400">
               a brand new website design
@@ -21,9 +21,9 @@ const LatestNews = () => {
             7 days ago
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row text-center md:text-left justify-between items-center py-4 border-b border-slate-100 ">
-          <img src={pic2} className="w-20 h-14 rounded-md" alt="pic1" />
-          <article className="capitalize">
+        <div className="flex flex-col sm:flex-row items-center py-4 text-center sm:text-left border-b border-slate-100 ">
+          <img src={pic2} className="w-36 h-20 mr-4 rounded-md" alt="pic1" />
+          <article className="capitalize grow">
             <h5 className="font-bold mt-4 sm:mt-1">changed the design</h5>
             <span className="text-sm text-zinc-400">
               a brand new website design
@@ -33,9 +33,9 @@ const LatestNews = () => {
             4 days ago
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row text-center md:text-left justify-between items-center py-4 border-b border-slate-100 ">
-          <img src={pic3} className="w-20 h-14 rounded-md" alt="pic1" />
-          <article className="capitalize">
+        <div className="flex flex-col sm:flex-row items-center py-4 text-center sm:text-left border-b border-slate-100 ">
+          <img src={pic3} className="w-36 h-20 mr-4 rounded-md" alt="pic1" />
+          <article className="capitalize grow">
             <h5 className="font-bold mt-4 sm:mt-1">changed the design</h5>
             <span className="text-sm text-zinc-400">
               a brand new website design
@@ -45,9 +45,9 @@ const LatestNews = () => {
             5 days ago
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row text-center md:text-left justify-between items-center py-4 border-b border-slate-100 ">
-          <img src={pic4} className="w-20 h-14 rounded-md" alt="pic1" />
-          <article className="capitalize">
+        <div className="flex flex-col sm:flex-row items-center py-4 text-center sm:text-left border-b border-slate-100 ">
+          <img src={pic4} className="w-36 h-20 mr-4 rounded-md" alt="pic1" />
+          <article className="capitalize grow">
             <h5 className="font-bold mt-4 sm:mt-1">changed the design</h5>
             <span className="text-sm text-zinc-400">
               a brand new website design
